0cf802164f10da6248377f2204989b976d0f7a2e
oibioib <pavelbodal@gmail.com>
2025-01-18 21:07:53 +0100

    feat: implement task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const { createReadStream } = require('fs');
+const { stdout } = require('process');
+
+const readStream = createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+readStream.pipe(stdout);
+
+readStream.on('error', (err) => {
+  console.log(err.message);
+});
